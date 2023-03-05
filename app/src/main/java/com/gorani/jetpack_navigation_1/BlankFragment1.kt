14582e46cad184fe1ede32f4b021712b04017594
gorani16 <jgee0116@naver.com>
2023-03-05 19:39:25 +0900

    Jetpack Navigation Graph

---
+++ app/src/main/java/com/gorani/jetpack_navigation_1/BlankFragment1.kt
@@ -0,0 +1,29 @@
+package com.gorani.jetpack_navigation_1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+
+class BlankFragment1 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_blank1, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.btn1).setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_blankFragment1_to_blankFragment2)
+        }
+
+    }
+
+}
